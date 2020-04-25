102b4b4b5c259bdf16d32a743472494074548420
Amadeusz Winogrodzki <a.winogrodzki@ocado.com>
2020-04-25 15:04:37 +0200

    chore: release

--- lib/react/inject.d.ts
+++ lib/react/inject.d.ts
@@ -2,5 +2,5 @@ import * as React from 'react';
 declare type PropsFromKeys<S, K extends keyof S> = {
     [P in K]: S[P];
 };
-export declare const createInject: <T extends object>() => <K extends keyof T>(...keys: K[]) => <P extends PropsFromKeys<T, K>>(Component: React.ComponentType<P>) => (props: Pick<P, Exclude<keyof P, K>>) => JSX.Element;
+export declare const createInject: <T extends object>() => <K extends keyof T>(...keys: K[]) => <P extends PropsFromKeys<T, K>>(Component: React.ComponentType<P>) => React.ForwardRefExoticComponent<React.PropsWithoutRef<Pick<P, Exclude<keyof P, K>>> & React.RefAttributes<unknown>>;
 export {};
--- lib/react/withContainer.d.ts
+++ lib/react/withContainer.d.ts
@@ -3,4 +3,4 @@ import { Container } from '../container';
 export interface ContainerProps<T> {
     container: Container<T>;
 }
-export declare const withContainer: <T, P extends ContainerProps<T>>(Component: React.ComponentType<P>) => (props: Pick<P, Exclude<keyof P, "container">>) => JSX.Element;
+export declare const withContainer: <T, P extends ContainerProps<T>>(Component: React.ComponentType<P>) => React.ForwardRefExoticComponent<React.PropsWithoutRef<Pick<P, Exclude<keyof P, "container">>> & React.RefAttributes<unknown>>;
