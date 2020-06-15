dbd712e9bfe053adf56e6c6236e2465dabb4f351
Amadeusz Winogrodzki <a.winogrodzki@ocado.com>
2020-06-15 20:03:56 +0200

    fix: export UseContainer interface

--- src/react/hook.ts
+++ src/react/hook.ts
@@ -2,7 +2,7 @@ import { ContainerContextValue, ContainerContext } from "./ContainerContext";
 import { useContext } from "react";
 import { Container } from "../container";
 
-interface UseContainer<T extends object> {
+export interface UseContainer<T extends object> {
   (): Container<T>;
   <K extends keyof T>(key: K): Pick<T, K>;
   <K extends keyof T>(key: K, ...keys: K[]): Pick<T, K>;
