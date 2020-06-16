d86d6c3f6d09dffbf152672667b4506cd37231d4
Amadeusz Winogrodzki <a.winogrodzki@ocado.com>
2020-06-16 13:18:58 +0200

    fixes

--- lib/react/hook.d.ts
+++ lib/react/hook.d.ts
@@ -1,8 +1,7 @@
 import { Container } from "../container";
-interface UseContainer<T extends object> {
+export interface UseContainer<T extends object> {
     (): Container<T>;
     <K extends keyof T>(key: K): Pick<T, K>;
     <K extends keyof T>(key: K, ...keys: K[]): Pick<T, K>;
 }
 export declare const createContainerHook: <T extends object>() => UseContainer<T>;
-export {};
